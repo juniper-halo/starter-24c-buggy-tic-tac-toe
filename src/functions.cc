58a5901bc4998c710cea2d899c2c583427d7c23e
juno <madhavt2@illinois.edu>
2025-02-16 03:07:56 +0000

    added right diagonal check

--- src/functions.cc
+++ src/functions.cc
@@ -53,6 +53,11 @@ char CheckWinner(const std::vector<std::vector<char>>& board) {
     }
   }
 
+  if (board[0][0] == board[1][1] && board[1][1] == board[2][2]
+      && board[0][2] != ' ') {
+    return board[0][0];
+  }
+
   if (board[0][2] == board[1][1] && board[1][1] == board[2][0] &&
       board[0][2] != ' ') {
     return board[0][0];
